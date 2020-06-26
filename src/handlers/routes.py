c41b82e42779b5d0806d296b25dbbde805ce6c03
Freddy da Paz Ilha <freddy.ilha@atlastechnol.com>
2020-06-26 00:12:01 -0300

    :construction: uploading file and learning to manipulate CSVs

--- src/handlers/routes.py
+++ src/handlers/routes.py
@@ -1,8 +1,24 @@
-from flask import render_template
+import os
+import csv
+from flask import render_template, request
+from pymongo import MongoClient
+from werkzeug.utils import secure_filename
 
 def routes_config(app):
 
+    
     @app.route('/', methods=['GET', 'POST'])
     def get_data():
-        data = "Hello, World"
-        return render_template('index.html', data=data)
+        return render_template('index.html')
+
+    @app.route('/file', methods=['POST'])
+    def save_file():
+        # csvFile = request.files['file'].read()
+        # test = csv.DictReader(csvFile)
+        # return render_template('editFile.html', file=test)
+
+
+        file = request.files['file'].read().decode('utf-8')
+        reader = csv.DictReader(file)
+        for row in reader:
+            return render_template('editFile.html', file=row)
--- src/server.py
+++ src/server.py
@@ -2,8 +2,8 @@ from flask import Flask
 from handlers.routes import routes_config
 
 app = Flask(__name__, template_folder='templates')
+app.config['UPLOAD_FOLDER'] = 'uploads'
 routes_config(app)
 
-# run the application
 if __name__ == "__main__":  
     app.run(host='0.0.0.0',debug=True,port=7676)
